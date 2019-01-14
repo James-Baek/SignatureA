bb6b1c9fc01012448d4bc60d32aec8a3df624047
James-Baek <ckdgus1428@gmail.com>
2019-01-15 01:58:12 +0900

    login2

--- SignatureProject/users/forms.py
+++ SignatureProject/users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+from allauth.account.forms import LoginForm
+
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
