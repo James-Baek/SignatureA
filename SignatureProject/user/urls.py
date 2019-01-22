1f8637810e25b4131352d1f68750962c0d9b4647
jobs0414 <tkddn262@naver.com>
2019-01-22 13:19:28 +0900

    latest

--- SignatureProject/user/urls.py
+++ SignatureProject/user/urls.py
@@ -4,9 +4,9 @@ from user.views import UserRegistrationView, UserLoginView, UserVerificationView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('signup/', UserRegistrationView.as_view()),
-    path('login/', UserLoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('signup/', UserRegistrationView.as_view(),name='signup'),
+    path('login/', UserLoginView.as_view(),name='login'),
+    path('logout/', LogoutView.as_view(),name='logout'),
     path('<pk>/verify/<token>/', UserVerificationView.as_view()),
     path('resend_verify_email/', ResendVerifyEmailView.as_view()),
 ]
