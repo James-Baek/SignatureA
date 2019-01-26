8d7c5f1744a0391218daf20a73529cb0d3ae222f
jobs0414 <tkddn262@naver.com>
2019-01-26 17:22:46 +0900

    sang sat finally

--- SignatureProject/photo/models.py
+++ SignatureProject/photo/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
 # Create your models here.
 # from __future__ import unicode_literals 
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models 
 from django.urls import reverse 
 from photo.fields import ThumbnailImageField #사진에 대한 원본 이미지와 썸네일 이미지 모두 저장 
-
 
 
 @python_2_unicode_compatible
@@ -24,11 +22,10 @@ class Album(models.Model):
         return reverse('photo:album_detail',args=(self.id,))
         
 @python_2_unicode_compatible
-
 class Photo(models.Model):
     album = models.ForeignKey(Album,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    image = ThumbnailImageField(upload_to='photo/%Y/%m')
+    image = ThumbnailImageField(upload_to='none')
     # image_test = models.ImageField(upload_to='null',blank=True,null=True)
     image_test2 = models.URLField(blank=True,null=True)
     description = models.TextField('photo Description',blank=True)
