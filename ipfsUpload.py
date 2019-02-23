3710c3b5f657a1958f288ccf8280520734997c4a
jobs0414 <tkddn262@naver.com>
2019-02-23 14:25:03 +0900

    merge

--- ipfsUpload.py
+++ ipfsUpload.py
@@ -1,12 +1,13 @@
 import subprocess
 import json
 
-print("start")
-result = subprocess.check_output(["node", "ipfsUpload.js", "./SignatureProject/media/documents/2019", "butterfly.mp3"]).decode('utf-8')
+# print("start")
 
+
+class HashMake():
+result = subprocess.check_output(["node", "ipfsUpload.js", "./SignatureProject/media/documents/2019", "butterfly.mp3"]).decode('utf-8')
 a = result.find("hash") + 7
 b = a + 46
-
-print(result[a:b])
-
 hash = result[a:b]
+# print(result[a:b])
+
